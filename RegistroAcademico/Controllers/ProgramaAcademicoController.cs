ac0cba3688d8568e6662b6f1bb845cb8fad24fae
lmra <laura.rodrigueza@udea.edu.co>
2018-03-24 12:25:26 -0500

    Trabajo actualizado en repositorio nuevo, RegistrosAcademicosOk

---
+++ RegistroAcademico/Controllers/ProgramaAcademicoController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RegistroAcademico.Controllers
+{
+    public class ProgramaAcademicoController : Controller
+    {
+        // GET: ProgramaAcademico
+        public ActionResult index()
+        {
+            return View();
+        }
+
+        public ActionResult crear()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ RegistroAcademico/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RegistroAcademico.Startup))]
+namespace RegistroAcademico
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
